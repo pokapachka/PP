bf0d09cf05f809943e506f429f51a3ed7e0444ba
pokapachka <pachkaosokin@yandex.ru>
2025-03-06 20:10:34 +0500

    Создание страницы Main и перехода со страницы MainWindow

--- Prog/Pages/Main.xaml.cs
+++ Prog/Pages/Main.xaml.cs
@@ -12,23 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Prog.Pages;
 
-namespace Prog
+namespace Prog.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Main.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Main : Page
     {
-        public MainWindow()
+        public Main()
         {
             InitializeComponent();
-            NavigatePage();
-        }
-        void NavigatePage()
-        {
-            frame.NavigationService.Navigate(new Main());
         }
     }
 }
